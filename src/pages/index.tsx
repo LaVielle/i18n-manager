44387f41635c3476b92455a56f8af005e28b719f
Samuel Mann <samuelcmann@gmail.com>
2021-05-22 21:01:31 +0200

    add some styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import translations from '../data.json'
 import { flattenObject } from '../utils/flattenObject'
 
-const normalizeDataShape = (flatObject: any) => {
+const normalizeDataShape = (flatObject: { [key: string]: string }) => {
   return Object.keys(flatObject).reduce((acc, cur, index) => {
     const [language, namespace, key] = cur.split('.')
 
@@ -48,43 +48,57 @@ export default function Index() {
 
   return (
     <div className="flex flex-row">
-      <div className="h-screen w-56 bg-blue-500 p-8 overflow-scroll">
+      <div className="h-screen w-56 bg-gray-800 p-8 overflow-scroll">
         {Object.keys(formatted).map((namespace) => (
-          <h1 key={`nameSpaceSidebar-${namespace}`}>{namespace}</h1>
+          <h1 key={`nameSpaceSidebar-${namespace}`} className="text-gray-100 text-xl mb-4">
+            {namespace}
+          </h1>
         ))}
       </div>
+
       <div className="flex flex-1 flex-col h-screen overflow-scroll p-8">
         {Object.keys(formatted).map((namespace, indexInNamespaces) => {
           const keysOfNamespace = Object.values(formatted)[indexInNamespaces]
 
           return (
-            <section className="pb-16 border-b-2 border-black mb-16">
-              <h2 className="text-4xl">{namespace}</h2>
+            <section
+              key={`namespaceSection-${namespace}`}
+              className="pb-16 border-b-2 border-gray-300 mb-16"
+            >
+              <h2 className="text-4xl mb-4">{namespace}</h2>
 
-              {Object.keys(keysOfNamespace).map((key, indexInKeys) => {
-                const languagesOfKey = Object.values(keysOfNamespace)[indexInKeys]
+              <div className="space-y-8">
+                {Object.keys(keysOfNamespace).map((key, indexInKeys) => {
+                  const languagesOfKey = Object.values(keysOfNamespace)[indexInKeys]
 
-                return (
-                  <div className="mt-4">
-                    <h3 className="text-2xl">{key}</h3>
+                  return (
+                    <div
+                      key={`namespaceKeySection-${namespace}-${key}`}
+                      className="p-4 bg-gray-200 rounded-lg shadow-sm"
+                    >
+                      <h3 className="text-2xl text-gray-900">{key}</h3>
 
-                    {Object.keys(languagesOfKey).map((language, indexInLanguages) => {
-                      const translation = Object.values(languagesOfKey)[indexInLanguages]
+                      {Object.keys(languagesOfKey).map((language, indexInLanguages) => {
+                        const translation = Object.values(languagesOfKey)[indexInLanguages]
 
-                      return (
-                        <div className="flex items-center mt-4">
-                          <p className="mr-4 text-xl text-gray-700">{language}</p>
-                          <input
-                            className="flex-1 border-2 p-4 rounded-md border-gray-300"
-                            defaultValue={translation}
-                            onChange={(e) => console.log('onChange', e.target.value)}
-                          />
-                        </div>
-                      )
-                    })}
-                  </div>
-                )
-              })}
+                        return (
+                          <div
+                            key={`namespaceKeyLanguageSection-${namespace}-${key}-${language}-`}
+                            className="flex items-center mt-4"
+                          >
+                            <p className="mr-4 text-xl text-gray-700">{language}</p>
+                            <input
+                              className="flex-1 border-2 p-4 rounded-md border-gray-300"
+                              defaultValue={translation}
+                              onChange={(e) => console.log('onChange', e.target.value)}
+                            />
+                          </div>
+                        )
+                      })}
+                    </div>
+                  )
+                })}
+              </div>
             </section>
           )
         })}
